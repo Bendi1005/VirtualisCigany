5eeb23af4987dc44fbecf1fac8b642281a8226a3
Molnár Bendegúz <93580791+Bendi1005@users.noreply.github.com>
2022-11-15 09:27:05 +0100

    a lore hozzáadva

---
+++ functions.py
@@ -0,0 +1,12 @@
+import os
+
+def intro():
+    os.system('cls')
+    print("(MESÉLŐ):    Fárasztó nap volt, nemde? Éppen megérkeztél Miskolcról a Nyugati pályaudvarra.")
+    print("Már nagyon fáradt vagy, leterhelt a hétvége amit ott töltöttél, így minél hamarabb otthon akarsz lenni.")
+    print("Az út viszont még hosszú hazáig, először a Nyugati aluljáróban találod magad.")
+    print(F"\nA cigányok megtalálnak és az egyik cigarettát kér tőled, mit válaszolsz?")
+    print(F"\n1 - Ittvan testvér, szívjad egészséggel!")
+    print("2 - Sajnálon, de sietek\n")
+
+
