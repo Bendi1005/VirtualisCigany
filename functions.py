671c452e91e970c0c0fe834556061136bbf0fe6a
Molnár Bendegúz <93580791+Bendi1005@users.noreply.github.com>
2022-11-15 10:17:42 +0100

    szarul működö while hozzáadva

--- functions.py
+++ functions.py
@@ -1,12 +1,18 @@
 import os
 
+state = 0
+
 def intro():
+
     os.system('cls')
     print("(MESÉLŐ):    Fárasztó nap volt, nemde? Éppen megérkeztél Miskolcról a Nyugati pályaudvarra.")
     print("Már nagyon fáradt vagy, leterhelt a hétvége amit ott töltöttél, így minél hamarabb otthon akarsz lenni.")
     print("Az út viszont még hosszú hazáig, először a Nyugati aluljáróban találod magad.")
     print(F"\nA cigányok megtalálnak és az egyik cigarettát kér tőled, mit válaszolsz?")
     print(F"\n1 - Ittvan testvér, szívjad egészséggel!")
-    print("2 - Sajnálon, de sietek\n")
+    print("2 - Sajnálon, de sietek!\n")
+state += 1
+
+
 
 
--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 from functions import *
 
 
-print({intro()})
+intro()
+
+print
+
+while True:
+	cigiOrNot = input()
+	try:
+		if int(cigiOrNot) == 1:
+			print("A jóember megköszöni a cigarettát és rágyujt.")
+			print("Te folytatod utadat hazafelé, irány a 3as metró.")
+			break
+		elif int(cigiOrNot) == 2:
+			print("A testvérek körbeállnak, majd az egyik közelebblép és vesénszúr.")
+			print(f"A sérülésed olyan súlyos hogy a kiérkező mentők nem tudnak megmenteni.\n\n GAME OVER")
+			break
+		else:
+			print("Csak '1' vagy '2' az elfogadható válasz.")
+	except:
+		print("Csak teljes érték fogadható el.")
+
+
+
+
+
+
+
+
+
