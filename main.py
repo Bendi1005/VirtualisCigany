8d831e57fa4d014ecfa4b7df86d31993f3ab59a9
Molnár Bendegúz <93580791+Bendi1005@users.noreply.github.com>
2022-11-15 11:20:17 +0100

    részlegesen javítva

--- main.py
+++ main.py
@@ -1,40 +1,25 @@
-from functions import *
+# from functions import *
 
 
-intro()
+# intro()
 
-print
+cigiOrNot='0'
 
-cigiOrNot=0
+vege = False
+# while not vege:
+while not vege:
+    cigiOrNot = input('Válasz: ')
+    if cigiOrNot == '1':
+        print("A jóember megköszöni a cigarettát és rágyujt.")
+        print("Te folytatod utadat hazafelé, irány a 3as metró.")
+    elif cigiOrNot == '2':
+        print("A testvérek körbeállnak, majd az egyik közelebblép és vesénszúr.")
+        print(f"A sérülésed olyan súlyos hogy a kiérkező mentők nem tudnak megmenteni.\n\n GAME OVER")
+        vege = True
+    else:
+        print("Csak '1' vagy '2' az elfogadható válasz.")
 
-while True:
-	cigiOrNot = input()
-	try:
-		if int(cigiOrNot) == 1:
-			print("A jóember megköszöni a cigarettát és rágyujt.")
-			print("Te folytatod utadat hazafelé, irány a 3as metró.")
-			break
-		elif int(cigiOrNot) == 2:
-			print("A testvérek körbeállnak, majd az egyik közelebblép és vesénszúr.")
-			print(f"A sérülésed olyan súlyos hogy a kiérkező mentők nem tudnak megmenteni.\n\n GAME OVER")
-			break
-		else:
-			print("Csak '1' vagy '2' az elfogadható válasz.")
-	except:
-		print("Csak teljes érték fogadható el.")
-
-
-
-print("")
 print("Lassan lesétálsz a 3as metróba, majd elindulsz Kőbánya-Kispest irányába.")
-print("")
-print(" ")
-print(" ")
-print(" ")
-print(" ")
-print("")
-print("")
-
 
 
 
